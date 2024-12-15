6361a9b154eb08917f0737c214944ec89609577f
cawallman <152647134+cawallman@users.noreply.github.com>
2024-12-15 12:30:59 -0500

    Update StateMachine1.ino

--- StateMachine1.ino
+++ StateMachine1.ino
@@ -36,8 +36,8 @@ int soundCount = 0;
 int sweeper = 20;
 
 int movePrev = 0;
-int pathColorsSize = 4;
-int pathColors[4];
+int pathColorsSize = 3;
+int pathColors[3];
 int falls = 0;
 int findRed = 0;
 
@@ -47,10 +47,11 @@ int colorFindYellow = 0;
 bool foundWallYellow = false;
 
 int prevColor = 0;
-int delay90 = 1700;
+int delay90 = 1350;
 int delay180 = 2*delay90;
 int sameCount = 0;
 int robustcolor = 0;
+String ColorNamesString[4] = {"Yellow","Black","Red","Blue"};
 
 
 
@@ -62,29 +63,29 @@ void setup() {
   
   MP3player.begin(mySerial);
 
-  MP3player.volume(0);
-  // delay(500);
-  // while (status != WL_CONNECTED) {
-  //   Serial.print("Attempting to connect to Network named: ");
-  //   Serial.println(ssid);                   // print the network name (SSID);
+  MP3player.volume(30);
+  delay(500);
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
 
-  //   // Connect to WPA/WPA2 network:
-  //   status = WiFi.begin(ssid, pass);
-  // }
-  // // print the SSID of the network you're attached to:
-  // Serial.print("SSID: ");
-  // Serial.println(WiFi.SSID());
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+  }
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
 
-  // // print your WiFi shield's IP address:
-  // IPAddress ip = WiFi.localIP();
-  // Serial.print("IP Address: ");
-  // Serial.println(ip);
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 
-  // Serial.println("starting WebSocket client");
-  // client.begin();
-  // client.beginMessage(TYPE_TEXT);
-  // client.print(clientID);
-  // client.endMessage();
+  Serial.println("starting WebSocket client");
+  client.begin();
+  client.beginMessage(TYPE_TEXT);
+  client.print(clientID);
+  client.endMessage();
 
   pinMode(2, INPUT);
   pinMode(3, OUTPUT);
@@ -102,98 +103,156 @@ void setup() {
 
 
 void loop() {
-  
-    if (stateVariable == 0) {
-      // fwd until wall
+
+    // int messageSize = client.parseMessage();
+  //   if (messageSize > 0) {
+  //     Serial.println("Received a message:");
+  //     String message = client.readString();
+  //     Serial.println(message);
+  //     if (message == "WebClient_4A9EDB0160D5.motors.255.-255") {
+  //       forward();
+  //     }
+  //     else if (message == "WebClient_4A9EDB0160D5.motors.-255.255") {
+  //       backward();
+  //     }
+  //     else if (message == "WebClient_4A9EDB0160D5.motors.0.200") {
+  //       leftTurn();
+  //     }
+  //     else if (message == "WebClient_4A9EDB0160D5.motors.0.0") {
+  //       stopAll();
+  //     }
+  //     else {
+  //       stopAll();
+  //     }
+  //   }
       Serial.print("stateVariable: ");
       Serial.println(stateVariable);
+  if (stateVariable == 0) {
+      // fwd until wall
       forward();
       if (wallSensor()) {
         stopAll();
-        delay(10);
+        delay(100);
         stateVariable = 1;
       }
   } 
   else if (stateVariable == 1){
   // flip around
-      rightTurn();
-      delay(delay180);
+      leftTurn();
+      delay(delay180 + 800);
       stopAll();
       stateVariable = 2;
   } 
-  else if (stateVariable = 2){
+  else if (stateVariable == 2){
     // fwd until red
       forward();
-      int color = robustColorSensing(currIndex);
-      Serial.println("COLOR: ");
-      
-      if (color == 3) {
+      int redcolor = robustColorSensing(currIndex);
+
+      if (redcolor == 4) {
+        // stopAll();
+        // delay(100);
+        playBlue();
+        Serial.println("TURNING...");
         leftTurn();
         delay(delay90);
         stopAll();
-        //signal to bot 2 that red is found here
+        delay(100);
         stateVariable = 3;
+        // return;
+        //signal to bot 2 that red is found here
       }
     } 
-    else if (stateVariable = 3){// lane follow red
-      laneFollow(3); 
+    else if (stateVariable == 3){// lane follow red
+      Serial.println("LANE FOLLOWING RED");
+      laneFollow(4); 
       if (wallSensor()) {
-        leftTurn();
-        delay(delay90);
+        rightTurn();
+        delay(delay90 + 100);
         stopAll();
+        delay(300);
         stateVariable = 4;
       }
     }
-    else if (stateVariable = 4){  // go fwd to find yellow
+    else if (stateVariable == 4){  // go fwd to find yellow
       forward();
-      colorFindYellow = robustColorSensing(currIndex);
+      //stopAll();
+      //int colorFindYellow = robustColorSensing(currIndex);
+      
+      colorSensing(Sensing, currIndex);
+      int colorFindYellow = pathColors[currIndex];
+      // Serial.println(colorFindYellow);
       if (colorFindYellow == 1) {
-        leftTurn();
-        delay(delay90);
+        forward();
+        delay(500);
+        playYellow();
+        rightTurn();
+        delay(delay90 + 800);
         stopAll();
+        delay(100);
+        forward();
+        delay(50);
+        stopAll();
+        delay(50);
         stateVariable = 5;
       }
     }
-    else if (stateVariable = 5){  // lane follow yellow
+    else if (stateVariable == 5){  // lane follow yellow
       // WAIT UNTIL RECIEVED SIGNAL FROM BOT 2
+      Serial.println("LANE FOLLOWING YELLOW");
       laneFollow(1); 
       if (wallSensor()) {
-        leftTurn();
+        rightTurn();
         delay(delay90);
         stopAll();
+        delay(100);
         stateVariable = 6;
         // tell bot 2 we have returned
       }
     }
-    else if (stateVariable = 6){
+    else if (stateVariable == 6){
     forward();
     if (wallSensor()) {
       stopAll();
       stateVariable = 7;
       }
     }
-    else if (stateVariable = 7){ // idle
+    else if (stateVariable == 7){ // idle
       // WAIT to ACKNOWLEDGE BOT2 RETURN
       stopAll();
     }
     else {
       stopAll();
     }
-  // Serial.println("COLOR: ");
-  // Serial.println(r);
-  // Serial.println(robustColorSensing(currIndex));
-  // robustColorSensing(currIndex);
+  // // Serial.println("COLOR: ");
+  // // Serial.println(r);
+  // // Serial.println(robustColorSensing(currIndex));
+  // // robustColorSensing(currIndex);
+  //colorSensing(Sensing, currIndex);
+  //   for (int i = 0; i < 3; i++){
+  
+  //     Serial.print(Sensing[i]);
+  //     Serial.print("  :  ");
+  //   }
+  //   Serial.print(" diff: ");
+  // Serial.println(Sensing[0]-Sensing[1]);
+  //Serial.println(ColorNamesString[pathColors[currIndex] - 1]);
+  // for (int i = 0; i < pathColorsSize; i++){
+  
+  // Serial.print(pathColors[i]);
+  // Serial.print("  :  ");
+  // }
+  //Serial.print("\n");
   currIndex++;
   if (currIndex > 3) {
     currIndex = 0;
   }
-
+ //delay(200);
 }
 
 bool wallSensor() {
   float wallSense = analogRead(A5) * (5.0/1023);
   bool wall = false;
-  if (wallSense >= 1.0) {
+  if (wallSense >= 1.2) {
     wall = true;
     Serial.println("Detected wall (in wall sensor function)");
   }
@@ -202,15 +261,13 @@ bool wallSensor() {
 }
 
 void colorSensing(float* Sensing, int currIndex) {
-  
   // Sensing stores [red,blue,yellow]
   int plex[2] = {1,0};
-  digitalWrite(BlueLEDpinTest1, 0);
-  digitalWrite(RedLEDpinTest1, 0);
-  delay(50);
-  float ambientValue = analogRead(A0) * (5.0/1023);
-  delay(10);
-  Sensing[2] = ambientValue;
+  
+  // delay(50);
+  // float ambientValue = analogRead(A0) * (5.0/1023);
+  // delay(10);
+ // Sensing[2] = ambientValue;
   for (int i = 0; i < 2; i++) {
     
     digitalWrite(RedLEDpinTest1, plex[i]);
@@ -219,8 +276,10 @@ void colorSensing(float* Sensing, int currIndex) {
     delay(50);
     float sensorValue = analogRead(A0) * (5.0/1023);
     
-    Sensing[i] = sensorValue/ambientValue;
-    delay(10);       
+    Sensing[i] = sensorValue;///ambientValue;
+    delay(10);    
+    digitalWrite(BlueLEDpinTest1, 0);
+    digitalWrite(RedLEDpinTest1, 0);   
   }
   
 
@@ -228,15 +287,18 @@ void colorSensing(float* Sensing, int currIndex) {
   int color = 0;
   // Serial.print("Diff Value: ");
   // Serial.println(diff);
-  if ((fabs(diff) < 3 && fabs(diff) > 0.2)) {
-    color = 2; //black
-  } else if (fabs(diff) < 0.15) {
+  if (fabs(diff) < 1) {
+    if (Sensing[0] < 2 && Sensing[1] < 2){
+      color = 2;//black
+    } else {
     color = 1; //yellow
-  } else if (diff > 3.1) {
-    color = 3;//red 
-  } else if (diff < -8) {
-    color = 4; //blue
+    }
+  } else if (diff > 0 ) {
+    color = 3; //red
+  } else if (diff < 0) {
+    color = 4;//blue 
   }
+
   pathColors[currIndex] = color;
 }
 
@@ -271,23 +333,28 @@ int robustColorSensing(int currIndex) {
 
   }
 
+  // Serial.println(ColorNamesString[robustcolor-1]);
+
   return robustcolor;
 }
 
 
-int sweeperNumber = 9;
+int sweeperNumber = 6;
 
 void laneFollow(int colorOfLane) {
   // float Sensing[3];
-  int color = robustColorSensing(currIndex);
-    if (color == colorOfLane) {
+  colorSensing(Sensing, currIndex);
+  int color = pathColors[currIndex];
+  // Serial.print("COLOR LANE FOLLOWING: ");
+  // Serial.println(color);
+  if (color == colorOfLane) {
     sweeper = sweeperNumber;
     forward();
   } else {
       if(color != prevColor) {
         falls++;
       }
-      if(sweeper > sweeperNumber*2/3) {
+      if(sweeper > sweeperNumber*1/2) {
         if (falls % 2 == 0) {
           leftTurn();
         }
@@ -440,7 +507,7 @@ void changeState2() {
 }
 
 
-int motorSpeed = 50;
+int motorSpeed = 70;
 void forwardRight() {
   analogWrite(10, motorSpeed);
   digitalWrite(3, LOW);
